c040a4cf6fc8892c243467b45f780b72652c04a8
soyekwon <soye0710@naver.com>
2021-11-14 23:16:10 +0900

    ps:dfs

---
+++ DFS/3184.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+int r, c;
+int o, v, to, tv = 0;
+char map[300][300];
+
+
+void dfs(int x, int y){
+	map[x][y] = '#';
+	if(x-1>=0 && map[x-1][y]!='#'){
+		if(map[x-1][y]=='o'){
+			o++;
+		}
+		else if(map[x-1][y]=='v'){
+			v++;
+		}
+		dfs(x-1,y);
+	}
+	if(x+1<r && map[x+1][y]!='#'){
+		if(map[x+1][y]=='o'){
+			o++;
+		}
+		else if(map[x+1][y]=='v'){
+			v++;
+		}
+		dfs(x+1,y);
+	}
+	if(y-1>=0 && map[x][y-1]!='#'){
+		if(map[x][y-1]=='o'){
+			o++;
+		}
+		else if(map[x][y-1]=='v'){
+			v++;
+		}
+		dfs(x,y-1);
+	}
+	if(y+1<c && map[x][y+1]!='#'){
+		if(map[x][y+1]=='o'){
+			o++;
+		}
+		else if(map[x][y+1]=='v'){
+			v++;
+		}
+		dfs(x,y+1);
+	}
+	return;
+
+}
+
+int main(){
+	//freopen("input.txt","r",stdin);
+	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> r >> c;
+	
+	for(int i=0; i<r; i++){
+		for(int j=0; j<c; j++){
+			cin >> map[i][j]; 
+		}
+	}
+
+	for(int i=0; i<r; i++){
+		for(int j=0; j<c; j++){
+			if(map[i][j]!='#'){
+				if(map[i][j]=='o'){
+					o++;
+				}
+				else if(map[i][j]=='v'){
+					v++;
+				}
+				dfs(i,j);
+				if(v>=o){
+					tv += v;
+				}
+				else{
+					to += o;
+				}
+				o = 0;
+				v = 0;
+				
+			}
+		}
+	}
+	
+	cout << to << " " << tv;
+	
+}
