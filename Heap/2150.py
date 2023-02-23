a300d04c83230ab93db07966d5f2773005d7ecc5
soyekwon <soye0710@naver.com>
2023-02-23 17:37:06 +0900

    arr 이름 수정

--- Heap/2150.py
+++ Heap/2150.py
@@ -20,12 +20,12 @@ def dfs(node):  # 정방향 graph로 dfs
             dfs(i)
     stack.append(node)  # dfs탐색이 종료되는 순서대로 stack에 append
     
-def r_dfs(node, ssc):  # 역방향 graph로 dfs
+def r_dfs(node, scc):  # 역방향 graph로 dfs
     check[node] = 1
-    ssc.append(node)
+    scc.append(node)
     for i in r_graph[node]:
         if check[i] == 0:
-            r_dfs(i, ssc)
+            r_dfs(i, scc)
             
 # 모든 노드에 정방향 dfs 수행
 for i in range(1, v):
@@ -35,12 +35,12 @@ for i in range(1, v):
 check = [0] * (v + 1)  # 방문 체크 배열 초기화
 
 while stack:  # stack이 empty될 때까지 진행
-    ssc=[]
+    scc=[]
     node = stack.pop()  # stack에서 pop해서
     if check[node] == 0:
-        r_dfs(node, ssc)  # 역방향 dfs진행
-        ssc.sort()  # 오름차순으로 print해야 하므로 오름차순 sort
-        ans.append(ssc)  # 정답 배열에 append
+        r_dfs(node, scc)  # 역방향 dfs진행
+        scc.sort()  # 오름차순으로 print해야 하므로 오름차순 sort
+        ans.append(scc)  # 정답 배열에 append
 
 print(len(ans))
 ans.sort()
